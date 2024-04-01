551898bbad5b38ba4a6b87667ea43d7765902c09
albecabrera <albe.cabrera@yahoo.de>
2024-04-01 10:45:46 +0200

    changed 2 JS

---
+++ script.js
@@ -0,0 +1,36 @@
+// VARIABLES Y CONSTANTES
+
+// let miVariable = 45;
+// console.log("Mi edad es " + miVariable);
+
+// miVariable = 46;
+// console.log("Mi edad es " + miVariable);
+
+// let resultado = 2 + 2;
+// console.log("Mi resultado es: " + resultado);
+
+// LOS CONDICIONALES
+// let miNumero = 6;
+// let miNombre = "Alberto";
+// console.log(miNombre);
+// let resultadoPregunta = miNumero == 7;
+// console.log(resultadoPregunta);
+
+// if (miNumero !== 7) {
+//   console.log("Si mi numero vale 6");
+// } else {
+//   console.log("No, mi numero no vale 6");
+// }
+
+// BUCLES WHILE For
+// let contador = 1;
+// while (contador <= 10) {
+//   console.log("Hola a todos");
+//   contador++;
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log("Hola a todos");
+// }
+
+// FUNCIONES Y METODOS
